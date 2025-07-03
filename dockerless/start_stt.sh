a40a28a54910bde558c6d5adb89fddb5be16d8aa
Václav Volhejn <vaclav@kyutai.org>
2025-07-03 14:15:30 +0200

    Bump moshi-server version

--- dockerless/start_stt.sh
+++ dockerless/start_stt.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")/.."
 # A fix for building Sentencepiece on GCC 15, see: https://github.com/google/sentencepiece/issues/1108
 export CXXFLAGS="-include cstdint"
 
-cargo install --features cuda moshi-server@0.6.1
+cargo install --features cuda moshi-server@0.6.3
 moshi-server worker --config services/moshi-server/configs/stt.toml --port 8090
--- services/moshi-server/start_moshi_server_public.sh
+++ services/moshi-server/start_moshi_server_public.sh
@@ -6,6 +6,6 @@ export LD_LIBRARY_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_confi
 
 uvx --from 'huggingface_hub[cli]' huggingface-cli login --token $HUGGING_FACE_HUB_TOKEN
 
-CARGO_TARGET_DIR=/app/target cargo install --features cuda moshi-server@0.6.1
+CARGO_TARGET_DIR=/app/target cargo install --features cuda moshi-server@0.6.3
 
 /root/.cargo/bin/moshi-server $@
