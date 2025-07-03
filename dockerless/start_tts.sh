7b4d674ae9fc6d65eb65f4ff6a33bdd37c7effa2
Václav Volhejn <vaclav@kyutai.org>
2025-07-03 09:53:34 +0200

    Remove comment

--- dockerless/start_tts.sh
+++ dockerless/start_tts.sh
@@ -7,12 +7,3 @@ export CXXFLAGS="-include cstdint"
 
 cargo install --features cuda moshi-server@0.6.1
 moshi-server worker --config services/moshi-server/configs/tts.toml --port 8089
-
-
-# cd ../moshi-rs
-# cd moshi-server
-# uv sync
-# export PATH=$(pwd)/.venv/bin:$PATH
-# export LD_LIBRARY_PATH=$(python -c "from distutils.sysconfig import get_config_var as s; print(s('LIBDIR'))")
-# cd ..
-# cargo run --features=cuda --bin=moshi-server -r -- worker --config=moshi-server/config-py-swarm-small.toml --port 8089
