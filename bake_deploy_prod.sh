aae3224c5ffee37c641c1448de392620c65478c2
Václav Volhejn <vaclav@kyutai.org>
2025-10-03 11:47:13 +0200

    Try Gemma 12B

--- bake_deploy_prod.sh
+++ bake_deploy_prod.sh
@@ -25,8 +25,7 @@ fi
 set -x # Print commands
 
 export DOMAIN=unmute.sh
-# Note that using non-Mistral models also requires changing the vLLM args in ./swarm-deploy.yml
-export KYUTAI_LLM_MODEL=mistralai/Mistral-Small-3.2-24B-Instruct-2506
+export KYUTAI_LLM_MODEL=google/gemma-3-12b-it
 export DOCKER_HOST=ssh://root@${DOMAIN}
 
 echo "If you get an connection error, do: ssh root@${DOMAIN}"
